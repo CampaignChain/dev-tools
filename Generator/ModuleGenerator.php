5a91d06442962bda9f1a3e6d7ddbac90fb0f616e
Sandro Groganz <sandro@campaignchain.com>
2016-06-16 20:18:22 +0200

    CampaignChain/campaignchain-ce#74 Don't show done campaigns in drop-down for creating new Activity

--- Generator/ModuleGenerator.php
+++ Generator/ModuleGenerator.php
@@ -59,6 +59,7 @@ class ModuleGenerator extends BundleGenerator
         foreach ($modules as $module) {        
             $derivedClassName = $module['class_name'];
             if (strtolower($moduleType) == 'operation') {
+                $this->renderFile('job/Job.php.twig', $dir.'/Job/Job.php', array_merge($parameters, $module));
                 $this->renderFile('job/Report.php.twig', $dir.'/Job/' . $derivedClassName . 'Report.php', array_merge($parameters, $module));        
                 $this->renderFile('form/OperationType.php.twig', $dir.'/Form/Type/' . $derivedClassName . 'OperationType.php', array_merge($parameters, $module));        
                 $this->renderFile('entity/Entity.php.twig', $dir.'/Entity/' . $derivedClassName . '.php', array_merge($parameters, $module));
@@ -67,6 +68,9 @@ class ModuleGenerator extends BundleGenerator
             }
             if (strtolower($moduleType) == 'activity') {
                 $this->renderFile('controller/ActivityHandler.php.twig', $dir.'/Controller/' . $derivedClassName . 'Handler.php', array_merge($parameters, $module));
+            }
+            if (strtolower($moduleType) == 'campaign') {
+                $this->renderFile('job/Job.php.twig', $dir.'/Service/Job.php', array_merge($parameters, $module));
             }
             if (strtolower($moduleType) == 'channel') {
                 $this->renderFile('controller/ChannelController.php.twig', $dir.'/Controller/' . $derivedClassName . 'Controller.php', array_merge($parameters, $module));
@@ -89,6 +93,10 @@ class ModuleGenerator extends BundleGenerator
             // overwrite the default services.yml file created by the Symfony generator
             $this->renderFile('config/operation_services.yml.twig', $dir.'/Resources/config/services.yml', $parameters);
             $this->renderFile('views/fields.html.twig', $dir.'/Resources/views/Form/fields.html.twig', $parameters);        
+        }
+        if (strtolower($moduleType) == 'campaign') {
+            // overwrite the default services.yml file created by the Symfony generator
+            $this->renderFile('config/campaign_services.yml.twig', $dir.'/Resources/config/services.yml', $parameters);
         }
 
         $this->renderFile('config/campaignchain.yml.twig', $dir.'/campaignchain.yml', $parameters);
